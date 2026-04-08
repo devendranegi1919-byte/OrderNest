f68bf9786576122c342301461a587bd7078a8a37
devendranegi491-ux <devendra.negi491@gmail.com>
2026-04-09 01:17:53 +0530

    phase 1 initialization

---
+++ admin-service/src/test/java/com/ordernest/AdminServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ordernest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
